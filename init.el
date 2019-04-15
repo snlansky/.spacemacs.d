cc92827599feebfa1719cbbf4625d14cd0b7ca6d
snlan@live.cn <snlan@live.cn>
2019-04-15 20:44:43 +0800

    update

--- init.el
+++ init.el
@@ -306,6 +306,10 @@ before packages are loaded. If you are unsure, you should try in setting them in
   (setq tramp-ssh-controlmaster-options
         "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
   )
+  (setq configuration-layer-elpa-archives
+      '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+        ("org-cn"   . "http://elpa.emacs-china.org/org/")
+        ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
