7b65d16e737b46bc28c30031c3d41347f8181f78
snlan@live.cn <snlan@live.cn>
2019-04-18 10:54:51 +0800

    add rust model

--- init.el
+++ init.el
@@ -39,6 +39,7 @@ This function should only modify configuration layer settings."
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
+     rust
      javascript
      go
      ivy   ;; swiper research
