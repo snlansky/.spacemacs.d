96da0f33006c72ad9e84be610560660aed8c7adf
snlan@live.cn <snlan@live.cn>
2019-04-18 09:14:06 +0800

    newline indent

--- layers/snlan/config.el
+++ layers/snlan/config.el
@@ -2,6 +2,7 @@
                           (setq indent-tabs-mode 1)
                           (setq tab-width 4)))
 
+;; 回车缩进
 (defun disable-aindent()
   (clean-aindent-mode -1))
 (add-hook 'go-mode-hook 'disable-aindent)
