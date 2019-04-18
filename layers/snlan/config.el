b25f24d836630bfb422cd770c57bda4e17c1581d
snlan@live.cn <snlan@live.cn>
2019-04-18 09:13:44 +0800

    修改company select快捷键

--- layers/snlan/config.el
+++ layers/snlan/config.el
@@ -1,3 +1,7 @@
 (add-hook 'go-mode-hook (lambda()
                           (setq indent-tabs-mode 1)
                           (setq tab-width 4)))
+
+(defun disable-aindent()
+  (clean-aindent-mode -1))
+(add-hook 'go-mode-hook 'disable-aindent)
